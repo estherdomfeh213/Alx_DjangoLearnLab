90ff9542f99fefaf7468a3704031f0fdc350c0c6
estherdomfeh213 <domfehesther2020@gmail.com>
2024-12-23 10:34:32 +0000

    fix: api/views.py file configuration

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,6 +1,5 @@
-from django_filters.rest_framework import DjangoFilterBackend 
+from django_filters import rest_framework as filters
 from rest_framework import viewsets
-from rest_framework import filters
 from django.core.exceptions import ValidationError
 from rest_framework import generics 
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
