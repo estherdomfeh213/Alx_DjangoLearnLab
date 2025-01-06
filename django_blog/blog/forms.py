46fa7080f3ae928ef8b029325db6c07f72ba90f1
estherdomfeh213 <domfehesther2020@gmail.com>
2025-01-06 04:28:26 +0000

    add: form cofiguration for Post

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -1,8 +1,14 @@
 from django import forms
 from django.contrib.auth.models import User
 from .models import Profile
+from.models import Post
 
 class ProfileUpdateForm(forms.ModelForm):
     class Meta:
         model = Profile
         fields = ['bio', 'profile_picture']
+
+class PostForm(forms.ModelForm):
+  class Meta:
+    model = Post
+    fields = ['title', 'content']
