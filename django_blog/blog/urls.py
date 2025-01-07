b8f848bcc49714a57408a394d1175160d10864d5
estherdomfeh213 <domfehesther2020@gmail.com>
2025-01-07 11:00:54 +0000

    mod: url patterns

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
-from . import views 
-from .views import (
-    PostListView,
-    PostDetailView,
-    PostCreateView,
-    PostUpdateView,
-    PostDeleteView
-)
+from . import views
+ 
+# from .views import (
+#     PostListView,
+#     PostDetailView,
+#     PostCreateView,
+#     PostUpdateView,
+#     PostDeleteView
+# )
 
 app_name = 'blog'
 
@@ -15,10 +16,13 @@ urlpatterns = [
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
     path('profile/', views.profile, name='profile'),
-    path('post/', views.PostListView.as_view(), name='post_list'),  # List all posts
+    path('post/', views.PostListView.as_view(), name='post_list'),  
     path('post/<int:pk>/', views.PostDetailView.as_view(), name='post_detail'),
-    path('post/new/', views.PostCreateView.as_view(), name='post_create'),  # Create a new post
-    path('post/<int:pk>/update/', views.PostUpdateView.as_view(), name='post_edit'),  # Edit an existing post
-    path('post/<int:pk>/delete/', views.PostDeleteView.as_view(), name='post_delete'),  # Delete a post,
+    path('post/new/', views.PostCreateView.as_view(), name='post_create'),  
+    path('post/<int:pk>/update/', views.PostUpdateView.as_view(), name='post_edit'), 
+    path('post/<int:pk>/delete/', views.PostDeleteView.as_view(), name='post_delete'),
+    path('posts/<int:post_id>/comments/new/', views.add_comment, name='add-comment'),
+    path('comments/<int:pk>/edit/', views.edit_comment, name='edit-comment'),
+    path('comments/<int:pk>/delete/', views.CommentDeleteView.as_view(), name='delete-comment'),  
 
 ]
