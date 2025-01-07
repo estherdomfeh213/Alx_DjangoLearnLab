53fe9f6d862596bfb52a04775c3ce6cfade8bdcb
estherdomfeh213 <domfehesther2020@gmail.com>
2025-01-07 11:11:09 +0000

    fix: url file

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from . import views
- 
+from .views import (
+    CommentCreateView,
+    CommentUpdateView,
+    CommentDeleteView,
+)
 # from .views import (
 #     PostListView,
 #     PostDetailView,
@@ -24,5 +28,8 @@ urlpatterns = [
     path('posts/<int:post_id>/comments/new/', views.add_comment, name='add-comment'),
     path('comments/<int:pk>/edit/', views.edit_comment, name='edit-comment'),
     path('comments/<int:pk>/delete/', views.CommentDeleteView.as_view(), name='delete-comment'),  
+    path('post/<int:post_id>/comments/new/', CommentCreateView.as_view(), name='add-comment'),
+    path('comment/<int:pk>/update/', CommentUpdateView.as_view(), name='update-comment'),
+    path('comment/<int:pk>/delete/', CommentDeleteView.as_view(), name='delete-comment'),
 
 ]
