b7f7a86bb355f992730d0cccff1dfd6d01823fba
estherdomfeh213 <domfehesther2020@gmail.com>
2025-01-08 12:11:19 +0000

    mod files

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -9,6 +9,8 @@ from rest_framework.decorators import api_view
 from django.contrib.auth import get_user_model
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
+from .models import CustomUser
+from rest_framework.views import APIView
 
 
 class RegisterView(APIView):
@@ -32,83 +34,25 @@ class LoginView(APIView):
         return Response({'detail': 'Invalid credentials'}, status=status.HTTP_400_BAD_REQUEST)
 
 
-# @api_view(['POST'])
-# def follow_user(request, user_id):
-#     user_to_follow = get_user_model().objects.get(id=user_id)
-#     if user_to_follow != request.user:
-#         request.user.following.add(user_to_follow)
-#         return Response({"message": "Followed successfully"}, status=200)
-#     return Response({"message": "You cannot follow yourself"}, status=400)
-
-# @api_view(['POST'])
-# def unfollow_user(request, user_id):
-#     user_to_unfollow = get_user_model().objects.get(id=user_id)
-#     request.user.following.remove(user_to_unfollow)
-#     return Response({"message": "Unfollowed successfully"}, status=200)
-
-CustomUser = get_user_model()
-
-class FollowUserView(generics.GenericAPIView):
+class FollowUserView(APIView):
     permission_classes = [IsAuthenticated]
 
-    def post(self, request, user_id, *args, **kwargs):
-        # Get the user to follow
+    def post(self, request, user_id):
         try:
             user_to_follow = CustomUser.objects.get(id=user_id)
+            request.user.following.add(user_to_follow)  # Add user_to_follow to the following list
+            return Response({"message": f"You are now following {user_to_follow.username}"}, status=status.HTTP_200_OK)
         except CustomUser.DoesNotExist:
             return Response({"error": "User not found"}, status=status.HTTP_404_NOT_FOUND)
 
-        # Ensure that the user isn't following themselves
-        if user_to_follow == request.user:
-            return Response({"error": "You cannot follow yourself"}, status=status.HTTP_400_BAD_REQUEST)
-        
-        # Add user_to_follow to the current user's following list
-        request.user.following.add(user_to_follow)
-        return Response({"message": f"Successfully followed user {user_to_follow.username}"}, status=status.HTTP_200_OK)
-
-
-class UnfollowUserView(generics.GenericAPIView):
+class UnfollowUserView(APIView):
     permission_classes = [IsAuthenticated]
 
-    def post(self, request, user_id, *args, **kwargs):
-        # Get the user to unfollow
+    def post(self, request, user_id):
         try:
             user_to_unfollow = CustomUser.objects.get(id=user_id)
+            request.user.following.remove(user_to_unfollow)  # Remove user_to_unfollow from the following list
+            return Response({"message": f"You have unfollowed {user_to_unfollow.username}"}, status=status.HTTP_200_OK)
         except CustomUser.DoesNotExist:
             return Response({"error": "User not found"}, status=status.HTTP_404_NOT_FOUND)
 
-        # Ensure that the user isn't unfollowing themselves
-        if user_to_unfollow == request.user:
-            return Response({"error": "You cannot unfollow yourself"}, status=status.HTTP_400_BAD_REQUEST)
-        
-        # Remove user_to_unfollow from the current user's following list
-        request.user.following.remove(user_to_unfollow)
-        return Response({"message": f"Successfully unfollowed user {user_to_unfollow.username}"}, status=status.HTTP_200_OK)
-
-
-class UserFollowingListView(generics.GenericAPIView):
-    permission_classes = [IsAuthenticated]
-
-    def get(self, request, *args, **kwargs):
-        # Get all users that the current user is following
-        following = request.user.following.all()
-        following_list = [{"id": user.id, "username": user.username} for user in following]
-        return Response({"following": following_list}, status=status.HTTP_200_OK)
-
-
-class UserFeedView(generics.GenericAPIView):
-    permission_classes = [IsAuthenticated]
-
-    def get(self, request, *args, **kwargs):
-        # Get posts from users that the current user is following
-        following = request.user.following.all()
-        posts = []
-        for user in following:
-            user_posts = user.posts.all()  # Assuming the Post model is related to the user
-            posts.extend(user_posts)
-
-        # Sort posts by creation date (most recent first)
-        posts = sorted(posts, key=lambda post: post.created_at, reverse=True)
-        posts_list = [{"id": post.id, "title": post.title, "content": post.content, "author": post.author.username} for post in posts]
-
-        return Response({"feed": posts_list}, status=status.HTTP_200_OK)
